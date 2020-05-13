c33ffad05ab5402d25076b2fe8ea74f43839ce5f
Wade <wsw0615@gmail.com>
2020-05-13 13:57:13 +0800

    feat: basic RWD

--- scripts/app.js
+++ scripts/app.js
@@ -11,6 +11,7 @@ var app = new Vue({
     self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
     self.scanner.addListener('scan', function (content, image) {
       self.scans.unshift({ date: +(Date.now()), content: content });
+      alert('content',scan.content);
     });
     Instascan.Camera.getCameras().then(function (cameras) {//取得設備的相機數目
       self.cameras = cameras;
