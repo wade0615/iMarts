94ea97d0ecbc002208be22df8ed84dac846eb623
Wade <wsw0615@gmail.com>
2020-05-13 12:19:40 +0800

    feat: open main camera on mobile

--- scripts/app.js
+++ scripts/app.js
@@ -12,13 +12,17 @@ var app = new Vue({
     self.scanner.addListener('scan', function (content, image) {
       self.scans.unshift({ date: +(Date.now()), content: content });
     });
-    Instascan.Camera.getCameras().then(function (cameras) {
+    Instascan.Camera.getCameras().then(function (cameras) {//取得設備的相機數目
       self.cameras = cameras;
-      if (cameras.length > 0) {
+      if (cameras.length > 0) {//若設備相機數目大於0 則先開啟第0個相機
         self.activeCameraId = cameras[0].id;
         self.scanner.start(cameras[0]);
+      } 
+      if(cameras.length > 1) {
+        self.activeCameraId = cameras[1].id;
+        self.scanner.start(cameras[1]);
       } else {
-        console.error('No cameras found.');
+        console.error('No cameras found.');//若設備沒有相機數量則顯示"No cameras found";
       }
     }).catch(function (e) {
       console.error(e);
