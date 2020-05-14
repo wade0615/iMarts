50980086d33d95525795cb03e228c983c0a4c29f
Wade <wsw0615@gmail.com>
2020-05-14 11:13:22 +0800

    feat: split in vue component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
 console.log('I am inside');
+
+Vue.component('scan', {
+    data() {
+        return{
+            decodedContent: '',
+            errorMessage: ''
+        }
+    },
+    template: 
+        `<section>
+            <p>Last result: <b>{{ decodedContent }}</b></p>
+            <p class="error">{{ errorMessage }}</p>
+            <qrcode-stream @decode="onDecode" @init="onInit"></qrcode-stream>
+        </section>`,
+    methods: {
+        onDecode(content) {
+        this.decodedContent = content
+        },
+        onInit(promise) {
+            promise.then(() => {
+                console.log('Successfully initilized! Ready for scanning now!')
+                })
+            .catch(error => {
+                if (error.name === 'NotAllowedError') {
+                    this.errorMessage = 'Hey! I need access to your camera'
+                } else if (error.name === 'NotFoundError') {
+                    this.errorMessage = 'Do you even have a camera on your device?'
+                } else if (error.name === 'NotSupportedError') {
+                    this.errorMessage = 'Seems like this page is served in non-secure context (HTTPS, localhost or file://)'
+                } else if (error.name === 'NotReadableError') {
+                    this.errorMessage = 'Couldn\'t access your camera. Is it already in use?'
+                } else if (error.name === 'OverconstrainedError') {
+                    this.errorMessage = 'Constraints don\'t match any installed camera. Did you asked for the front camera although there is none?'
+                } else {
+                    this.errorMessage = 'UNKNOWN ERROR: ' + error.message
+                }
+                })
+            }
+        }
+})
+
+new Vue({
+    el: '#imarts',
+
+    data: {
+        title: 'iMarts 愛碼市',
+    },
+
+    methods: {
+        }
+});
+Vue.config.devtools = true;
